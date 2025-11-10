15bbb4f0ff121e5b73a6f8fce1b9d0ff520ba479
OPKovalev <kovalyovop@yandex.ru>
2025-11-10 21:33:05 +0300

    Проект теста на TaskEntityTest

---
+++ src/test/java/personal/opk/personalorganizationplanner/entity/TaskEntityTest.java
@@ -0,0 +1,43 @@
+package personal.opk.personalorganizationplanner.entity;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskEntityTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getCreatedAt() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setCreatedAt() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void builder() {
+    }
+}
